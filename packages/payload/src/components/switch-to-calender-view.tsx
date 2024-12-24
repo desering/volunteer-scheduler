c517fb3abf7296fefe6c2610c3b1581f713ce984
Richard Boomsma <richard.boomsma@proton.me>
2024-12-24 21:00:15 +0100

    refactor: simplify SwitchToCalenderView component by removing props and console log

--- packages/payload/src/components/switch-to-calender-view.tsx
+++ packages/payload/src/components/switch-to-calender-view.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 
-export const SwitchToCalenderView = (props) => {
-  console.log(Object.keys(props));
-  return <Link href="/admin/calender/shifts">Switch to calender view</Link>;
-};
+export const SwitchToCalenderView = () => (
+  <Link href="/admin/calender/shifts">Switch to calender view</Link>
+);
