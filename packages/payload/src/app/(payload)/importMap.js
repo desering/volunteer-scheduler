6495cf19c333ef413d35f452493116eef544487a
Richard Boomsma <spaaacetoast@proton.me>
2025-01-14 23:07:28 +0100

    update payload, fix event calender and on init page load, don't use animation for date row in overview (#33)
    Co-authored-by: Richard Boomsma <richard.boomsma@proton.me>

--- packages/payload/src/app/(payload)/importMap.js
+++ packages/payload/src/app/(payload)/importMap.js
@@ -23,7 +23,7 @@ import { ItalicFeatureClient as ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0
 import { PublishEventTemplate as PublishEventTemplate_1e3fcc053ab5e0eb55a23f0b1fa2bd4e } from 'src/components/publish-event-template'
 import { SwitchToCalenderView as SwitchToCalenderView_e5f36b9c46b66f6eca00310c58741220 } from 'src/components/switch-to-calender-view'
 import { DashboardHeader as DashboardHeader_0c59d0d196f0404bf841cd4d190d7596 } from '@/components/dashboard-header'
-import { CalenderView as CalenderView_be9dacc6b4cc95fb62b6a657f4b9b68a } from 'src/components/views/calender-view'
+import { CalenderView as CalenderView_747d4527aba6db24ae7b179ba05a3f0f } from 'src/views/calender-view'
 
 export const importMap = {
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
@@ -51,5 +51,5 @@ export const importMap = {
   "/components/publish-event-template#PublishEventTemplate": PublishEventTemplate_1e3fcc053ab5e0eb55a23f0b1fa2bd4e,
   "/components/switch-to-calender-view#SwitchToCalenderView": SwitchToCalenderView_e5f36b9c46b66f6eca00310c58741220,
   "@/components/dashboard-header#DashboardHeader": DashboardHeader_0c59d0d196f0404bf841cd4d190d7596,
-  "/components/views/calender-view#CalenderView": CalenderView_be9dacc6b4cc95fb62b6a657f4b9b68a
+  "/views/calender-view#CalenderView": CalenderView_747d4527aba6db24ae7b179ba05a3f0f
 }
