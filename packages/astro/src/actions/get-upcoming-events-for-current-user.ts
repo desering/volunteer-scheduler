1de0f3f4b9dcfc82b8ffa9c181e1c64a4dc48a6b
Richard Boomsma <spaaacetoast@proton.me>
2025-02-02 07:41:52 +0100

    Refactor upcoming events handling and improve styling (#71)

--- packages/astro/src/actions/get-upcoming-events-for-current-user.ts
+++ packages/astro/src/actions/get-upcoming-events-for-current-user.ts
@@ -1,36 +1,31 @@
 import type { Event } from "@payload-types";
-import { defineAction } from "astro:actions";
+import { ActionError, defineAction } from "astro:actions";
 import { startOfDay } from "date-fns";
-import { error } from "node:console";
-import { type DisplayableEvent, prepareEvent } from "~/utils/map-events";
+import { prepareEvent } from "~/utils/map-events";
 
 export const getUpcomingEventsForCurrentUser = defineAction({
   handler: async (_, context) => {
-    if (!context.locals.user) {
-      throw error("Not authenticated");
-    }
+    if (!context.locals.user)
+      throw new ActionError({
+        code: "UNAUTHORIZED",
+      });
 
     const signups = await context.locals.payload.find({
       collection: "signups",
       depth: 1,
-
       where: {
         user: { equals: context.locals.user.id },
         "event.start_date": { greater_than_equal: startOfDay(Date.now()) },
       },
-
+      sort: "event.start_date",
       pagination: false,
     });
 
-    return (
-      await Promise.all(
-        signups.docs
-          .map((s) => s.event as Event)
-          .map((event) => prepareEvent(event)),
-      )
-    ).sort(
-      (a: DisplayableEvent, b: DisplayableEvent) =>
-        a.start_date.getTime() - b.start_date.getTime(),
-    );
+    return {
+      signups,
+      events: await Promise.all(
+        signups.docs.map((s) => prepareEvent(s.event as Event)),
+      ),
+    };
   },
 });
