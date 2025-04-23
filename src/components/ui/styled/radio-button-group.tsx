a6d501672aaf73b98784f2e7405b686f4c1af432
Bernhard Frick <bernhard.frick@adevinta.com>
2025-04-23 12:04:37 +0200

    fix radio-button-group elements

--- src/components/ui/styled/radio-button-group.tsx
+++ src/components/ui/styled/radio-button-group.tsx
@@ -10,6 +10,7 @@ const { withProvider, withContext } = createStyleContext(radioButtonGroup);
 
 export type RootProps = ComponentProps<typeof Root>;
 export const Root = withProvider<
+  HTMLDivElement,
   Assign<
     Assign<HTMLPandaProps<"div">, RadioGroup.RootProps>,
     RadioButtonGroupVariantProps
@@ -17,22 +18,27 @@ export const Root = withProvider<
 >(RadioGroup.Root, "root");
 
 export const Indicator = withContext<
+  HTMLDivElement,
   Assign<HTMLPandaProps<"div">, RadioGroup.IndicatorProps>
 >(RadioGroup.Indicator, "indicator");
 
 export const ItemControl = withContext<
+  HTMLDivElement,
   Assign<HTMLPandaProps<"div">, RadioGroup.ItemControlProps>
 >(RadioGroup.ItemControl, "itemControl");
 
 export const Item = withContext<
+  HTMLDivElement,
   Assign<HTMLPandaProps<"label">, RadioGroup.ItemProps>
 >(RadioGroup.Item, "item");
 
 export const ItemText = withContext<
+  HTMLDivElement,
   Assign<HTMLPandaProps<"span">, RadioGroup.ItemTextProps>
 >(RadioGroup.ItemText, "itemText");
 
 export const Label = withContext<
+  HTMLDivElement,
   Assign<HTMLPandaProps<"label">, RadioGroup.LabelProps>
 >(RadioGroup.Label, "label");
 
