a5a61803e81c7ccf559d8213f1c0da3e3bb0472f
Richard Boomsma <richard.boomsma@proton.me>
2025-10-27 00:59:56 +0100

    fix: update Button component usage in NavBar for consistency

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,9 @@
+import Link from "next/link";
 import { Container } from "styled-system/jsx";
 import { panda } from "styled-system/jsx/factory";
-import { button } from "styled-system/recipes/button";
 import { ThemeSwitchButton } from "@/components/theme-switch-button";
 import { getUser } from "@/lib/services/get-user";
 import { Button } from "./ui/button";
-import { Link } from "./ui/link";
 
 export default async function NavBar() {
   const { user } = await getUser();
@@ -13,7 +12,7 @@ export default async function NavBar() {
     <Container marginTop="4" width="100%">
       <panda.nav display="flex" justifyContent="space-between">
         <panda.div display="flex" gap="4">
-          <Button variant="outline">
+          <Button asChild variant="outline">
             <Link href="/">Shifts</Link>
           </Button>
           {/* {user?.roles?.includes("admin") && (
@@ -27,15 +26,15 @@ export default async function NavBar() {
         <panda.div display="flex" gap="4">
           <ThemeSwitchButton />
           {user ? (
-            <Button variant="solid">
+            <Button asChild variant="solid">
               <Link href="/account">{user.preferredName}</Link>
             </Button>
           ) : (
             <>
-              <Button variant="outline">
+              <Button asChild variant="outline">
                 <Link href="/auth/sign-in">Sign in</Link>
               </Button>
-              <Button variant="solid">
+              <Button asChild variant="solid">
                 <Link href="/auth/register">Become a volunteer</Link>
               </Button>
             </>
