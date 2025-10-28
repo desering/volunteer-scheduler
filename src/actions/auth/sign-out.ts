e6533ea8e6617b06c09b0449e885a2e0f9603b3f
Richard Boomsma <richard.boomsma@proton.me>
2025-10-28 19:33:30 +0100

    fix: remove getUser caching

--- src/actions/auth/sign-out.ts
+++ src/actions/auth/sign-out.ts
@@ -3,8 +3,15 @@
 import config from "@payload-config";
 import { logout as payloadLogout } from "@payloadcms/next/auth";
 import { redirect } from "next/navigation";
+import { getUser } from "@/lib/services/get-user";
 
 export async function signOut() {
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/");
+  }
+
   try {
     await payloadLogout({ config });
   } catch (error) {
--- src/lib/services/get-user.ts
+++ src/lib/services/get-user.ts
@@ -1,11 +1,10 @@
 import config from "@payload-config";
 import { headers as getHeaders } from "next/headers";
 import { getPayload } from "payload";
-import { cache } from "react";
 
-export const getUser = cache(async () => {
+export const getUser = async () => {
   const headers = await getHeaders();
   const payload = await getPayload({ config });
 
   return await payload.auth({ headers });
-});
+};
