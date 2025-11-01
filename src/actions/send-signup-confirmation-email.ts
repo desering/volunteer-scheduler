68ac7a11afe72afe50006abe0a663184a793b28a
Bernhard Frick <bernhard.frick@adevinta.com>
2025-11-01 04:38:34 +0100

    implement working email sending

--- src/actions/send-signup-confirmation-email.ts
+++ src/actions/send-signup-confirmation-email.ts
@@ -1,59 +1,44 @@
 "use server";
 
 import config from "@payload-config";
-import { getPayload } from "payload";
-import ical, { ICalCalendarMethod, ICalDescription } from "ical-generator";
-
-// https://nodemailer.com/
-// https://payloadcms.com/docs/email/overview
-// https://github.com/sebbo2002/ical-generator
-// https://github.com/payloadcms/payload/tree/main/packages/email-nodemailer
+import { getPayload, SendEmailOptions } from "payload";
+import ical, { ICalCalendarMethod, ICalEventData } from "ical-generator";
 
 export const sendSignupConfirmationEmail = async () => {
   const calendar = ical({
-    name: 'my first iCal',
-    method: ICalCalendarMethod.REQUEST, // required for email client to display event as an invitation
+    name: "De Sering Calendar",
+    method: ICalCalendarMethod.REQUEST,
   });
 
-  const event = calendar.createEvent({
-    description(): ICalDescription | null {
-      return {
-        plain: "",
-        html: "",
-      };
-    },
-    summary(): string {
-      return "";
-    },
-  });
+  const start = new Date();
+  const end = new Date();
+  end.setHours(start.getHours() + 1);
 
-  // calendar.createEvent({
-  //     start: new Date(),
-  //     end: new Date().setHours(startTime.getHours()+1),
-  //     summary: 'Example Event',
-  //     description: 'It works ;)',
-  //     location: 'my room',
-  //     url: 'http://sebbo.net/'
-  // });
+  calendar.createEvent({
+    summary: "Tuesday Evening First Shift",
+    description: {
+      plain: "plain event description",
+    },
+    start: start,
+    end: end,
+    location: "De Sering, Rhoneweg 6, 1043 AH Amsterdam",
+    organizer: "schedule@desering.org",
+  } as ICalEventData);
 
-  let message = {
-    to: "frickb95@gmail.com",
-    headers: {
-      "x-invite": {
-        prepared: true,
-        value: id,
+  let message: SendEmailOptions = {
+    from: "De Sering Schedule <schedule@desering.org>",
+    to: "TODO: RECIPIENT HERE",
+    subject: "Shift Signup Notification",
+    text: "This is a plain text body",
+    html: "<p>This a html body</p>",
+    attachments: [
+      {
+        content: calendar.toString(),
+        contentType: "text/calendar",
       },
-    },
-    icalEvent: {
-      filename: "invite.ics",
-      method: "PUBLISH", // REQUEST
-      content: event.toString(),
-    },
-    subject: "This is a test email",
-    text: "This is my message body",
+    ],
   };
 
   const payload = await getPayload({ config });
-  const email = await payload.sendEmail(message);
-  console.log(email);
+  await payload.sendEmail(message);
 };
