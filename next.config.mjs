2bd9fbe59fb72735752dded24ce6b3a626dcb0ef
Bernhard Frick <bernhard.frick@adevinta.com>
2025-11-09 00:13:09 +0100

    chore: enable react strict mode

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  reactStrictMode: true,
 };
 
 export default withPayload(nextConfig);
