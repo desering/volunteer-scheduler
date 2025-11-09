77ed02c0f6ff68e7c4bb8b49c47b561ff6cb878c
Bernhard Frick <bernhard.frick@adevinta.com>
2025-11-09 11:31:58 +0100

    separate functions for create invite and send email

--- src/actions/send-signup-confirmation-email.ts
+++ src/actions/send-signup-confirmation-email.ts
@@ -1,44 +1,84 @@
 "use server";
 
 import config from "@payload-config";
-import { getPayload, SendEmailOptions } from "payload";
-import ical, { ICalCalendarMethod, ICalEventData } from "ical-generator";
+import type { Signup } from "@payload-types";
+import ical, {
+  type ICalCalendar,
+  ICalCalendarMethod,
+  type ICalEventData,
+} from "ical-generator";
+import { getPayload, type SendEmailOptions } from "payload";
 
-export const sendSignupConfirmationEmail = async () => {
+type CreateCalendarInviteParams = {
+  summary: string;
+  description?: string;
+  start: Date;
+  end: Date;
+  location?: string;
+};
+const createCalendarInvite = (
+  params: CreateCalendarInviteParams,
+): ICalCalendar => {
   const calendar = ical({
-    name: "De Sering Calendar",
+    name: "De Sering",
     method: ICalCalendarMethod.REQUEST,
   });
+
+  calendar.createEvent({
+    summary: params.summary,
+    description: {
+      plain: params.description,
+    },
+    start: params.start,
+    end: params.end,
+    location: params.location,
+    organizer: "schedule@desering.org",
+  } as ICalEventData);
+
+  return calendar;
+};
+
+type SendEmailParams = SendEmailOptions;
+const sendEmail = async (params: SendEmailParams) => {
+  const payload = await getPayload({ config });
+
+  return payload.sendEmail({
+    from: "De Sering Schedule <schedule@desering.org>",
+    ...params,
+  });
+};
+
+type SendSignupConfirmationEmailParams = {
+  signup: Signup;
+};
+
+export const sendSignupConfirmationEmail = async (
+  params: SendSignupConfirmationEmailParams,
+) => {
+  console.log(params);
 
   const start = new Date();
   const end = new Date();
   end.setHours(start.getHours() + 1);
 
-  calendar.createEvent({
+  const invite = createCalendarInvite({
     summary: "Tuesday Evening First Shift",
-    description: {
-      plain: "plain event description",
-    },
+    description: "plain event description",
     start: start,
     end: end,
     location: "De Sering, Rhoneweg 6, 1043 AH Amsterdam",
-    organizer: "schedule@desering.org",
-  } as ICalEventData);
+  });
 
-  let message: SendEmailOptions = {
-    from: "De Sering Schedule <schedule@desering.org>",
+  const email = await sendEmail({
     to: "TODO: RECIPIENT HERE",
     subject: "Shift Signup Notification",
     text: "This is a plain text body",
     html: "<p>This a html body</p>",
     attachments: [
       {
-        content: calendar.toString(),
+        content: invite.toString(),
         contentType: "text/calendar",
       },
     ],
-  };
-
-  const payload = await getPayload({ config });
-  await payload.sendEmail(message);
+  });
 };
