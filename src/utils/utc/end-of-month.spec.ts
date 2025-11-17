03772f16da65f0fdd834341b4ec795698a1115ee
Bernhard Frick <frickb95@gmail.com>
2025-11-17 23:50:57 +0100

    chore: add vitest (#183)
    add vitest

---
+++ src/utils/utc/end-of-month.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { endOfMonthUTC } from "./end-of-month";
+
+test("end of month", () => {
+  [
+    {
+      input: new Date("July 17, 2025, 13:24:00"),
+      expect: new Date("July 31, 2025, 23:59:59.999Z"),
+    },
+  ].forEach((c) => {
+    expect(endOfMonthUTC(c.input)).toEqual(c.expect);
+  });
+});
