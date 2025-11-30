0448461a16cd66489fff9431d9ec08725f8f2fd9
Bernhard Frick <bernhard.frick@adevinta.com>
2025-11-30 17:03:17 +0100

    fmt

--- src/lib/email/create-ical-event.spec.ts
+++ src/lib/email/create-ical-event.spec.ts
@@ -1,5 +1,5 @@
-import {expect, test, vi} from "vitest";
-import {createIcalEvent} from "./create-ical-event";
+import { expect, test, vi } from "vitest";
+import { createIcalEvent } from "./create-ical-event";
 
 test("base case", () => {
   const now = new Date("June 15, 2025, 12:34:56");
@@ -8,12 +8,16 @@ test("base case", () => {
   const start = new Date("July 17, 2025, 13:00:00");
   const end = new Date("July 17, 2025, 14:00:00");
 
-  expect(createIcalEvent({
-    id: "123-456",
-    summary: "Event title",
-    description: "Event description",
-    start,
-    end,
-    location: "Event location",
-  }).toString()).toEqual(`BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//sebbo.net//ical-generator//EN\r\nMETHOD:REQUEST\r\nNAME:De Sering\r\nX-WR-CALNAME:De Sering\r\nBEGIN:VEVENT\r\nUID:123-456\r\nSEQUENCE:0\r\nDTSTAMP:20250615T103456Z\r\nDTSTART:20250717T110000Z\r\nDTEND:20250717T120000Z\r\nSUMMARY:Event title\r\nLOCATION:Event location\r\nDESCRIPTION:Event description\r\nEND:VEVENT\r\nEND:VCALENDAR`);
+  expect(
+    createIcalEvent({
+      id: "123-456",
+      summary: "Event title",
+      description: "Event description",
+      start,
+      end,
+      location: "Event location",
+    }).toString(),
+  ).toEqual(
+    `BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//sebbo.net//ical-generator//EN\r\nMETHOD:REQUEST\r\nNAME:De Sering\r\nX-WR-CALNAME:De Sering\r\nBEGIN:VEVENT\r\nUID:123-456\r\nSEQUENCE:0\r\nDTSTAMP:20250615T103456Z\r\nDTSTART:20250717T110000Z\r\nDTEND:20250717T120000Z\r\nSUMMARY:Event title\r\nLOCATION:Event location\r\nDESCRIPTION:Event description\r\nEND:VEVENT\r\nEND:VCALENDAR`,
+  );
 });
