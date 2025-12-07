3f6752eff50d4fb625bca79fdb685b60935f6f7a
ericewe <theericewe@live.com>
2025-12-07 15:08:59 +0100

    feat: enable refetch event every minute

--- src/components/event-details-sheet/index.tsx
+++ src/components/event-details-sheet/index.tsx
@@ -45,6 +45,9 @@ export const EventDetailsDrawer = (props: Props) => {
       return await res.json();
     },
     enabled: !!props.eventId,
+    refetchInterval: 1000 * 60, // refetch every minute
+    refetchIntervalInBackground: true,
+    refetchOnWindowFocus: "always",
   });
 
   const [selectedRoleId, setSelectedRoleId] = useState<string>();
