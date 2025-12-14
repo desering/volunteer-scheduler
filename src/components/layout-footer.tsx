0ac948f7376af0c9295ead948774b06d9cc38302
ericewe <theericewe@live.com>
2025-12-14 15:13:35 +0100

    feat: add link to tech circle to layout footer

--- src/components/layout-footer.tsx
+++ src/components/layout-footer.tsx
@@ -1,3 +1,4 @@
+import { css } from "styled-system/css";
 import { panda } from "styled-system/jsx";
 
 export const LayoutFooter = () => (
@@ -13,7 +14,20 @@ export const LayoutFooter = () => (
       >
         &#129654;
       </panda.span>
-      by volunteers at De Sering
+      by volunteers at De Sering &ndash;&nbsp;
+      <panda.a
+        href="https://wiki.desering.org/en/circles/tech"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={css({
+          textDecoration: "underline",
+          _hover: {
+            color: "gray.11",
+          },
+        })}
+      >
+        Join the Tech Circle to help!
+      </panda.a>
     </panda.p>
   </panda.footer>
 );
