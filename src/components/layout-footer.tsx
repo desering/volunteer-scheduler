823bc296953551c79f81771fc6301663c301d391
ericewe <theericewe@live.com>
2025-12-15 15:30:26 +0100

    feat: add padding and update heart color

--- src/components/layout-footer.tsx
+++ src/components/layout-footer.tsx
@@ -1,33 +1,35 @@
 import { css } from "styled-system/css";
-import { panda } from "styled-system/jsx";
+import { Container, panda } from "styled-system/jsx";
 
 export const LayoutFooter = () => (
-  <panda.footer display="flex" justifyContent="center">
-    <panda.p paddingBlock="10">
-      Made with
-      <panda.span
-        aria-label="love"
-        role="img"
-        paddingInline="2"
-        position="relative"
-        insetBlockStart="2px"
-      >
-        &#129654;
-      </panda.span>
-      by volunteers at De Sering &ndash;&nbsp;
-      <panda.a
-        href="https://wiki.desering.org/en/circles/tech"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={css({
-          textDecoration: "underline",
-          _hover: {
-            color: "gray.11",
-          },
-        })}
-      >
-        Join the Tech Circle to help!
-      </panda.a>
-    </panda.p>
-  </panda.footer>
+  <Container>
+    <panda.footer display="flex" justifyContent="center">
+      <panda.p paddingBlock="10" textAlign="center">
+        Made with
+        <panda.span
+          aria-label="love"
+          role="img"
+          paddingInline="2"
+          position="relative"
+          insetBlockStart="1px"
+        >
+          &#10084;&#65039;
+        </panda.span>
+        by volunteers at De Sering &ndash;&nbsp;
+        <panda.a
+          href="https://wiki.desering.org/en/circles/tech"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={css({
+            textDecoration: "underline",
+            _hover: {
+              color: "gray.11",
+            },
+          })}
+        >
+          Join the Tech Circle to help!
+        </panda.a>
+      </panda.p>
+    </panda.footer>
+  </Container>
 );
