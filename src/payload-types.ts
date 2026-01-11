80d071176a0d1499af40a34a47b0efe138814e01
ericewe <theericewe@live.com>
2026-01-11 15:21:33 +0100

    feat: add paylod types for total shifts

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -379,6 +379,7 @@ export interface Signup {
   role: number | Role;
   user: number | User;
   title?: string | null;
+  totalShifts?: number | null;
   updatedAt: string;
   createdAt: string;
 }
@@ -591,6 +592,7 @@ export interface SignupsSelect<T extends boolean = true> {
   role?: T;
   user?: T;
   title?: T;
+  totalShifts?: T;
   updatedAt?: T;
   createdAt?: T;
 }
