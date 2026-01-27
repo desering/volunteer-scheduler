3a263dac7a477fbe8400de1ea65a516a0db6ecf8
Bernhard Frick <bernhard.frick@adevinta.com>
2026-01-28 00:34:04 +0100

    chore: include jose in tracing

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,9 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  outputFileTracingIncludes: {
+    "*": ["node_modules/jose/**"],
+  },
 };
 
 export default withPayload(nextConfig);
