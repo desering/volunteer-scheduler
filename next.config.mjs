bb57d31b49da5f6695b1e0b1c5818a4832b8a845
Bernhard Frick <bernhard.frick@adevinta.com>
2026-01-27 23:05:09 +0100

    chore: upgrade to next.js 16

--- next.config.mjs
+++ next.config.mjs
@@ -8,9 +8,6 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default withPayload(nextConfig);
