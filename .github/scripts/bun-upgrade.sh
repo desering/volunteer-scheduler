a4a37d441d04b93f7d6b36c4e96b20d723ec8f7f
Mateusz Grzywacz <amateusz.grzywacz@gmail.com>
2026-02-17 14:49:01 +0100

    fix dependabot script and condition

--- .github/scripts/bun-upgrade.sh
+++ .github/scripts/bun-upgrade.sh
@@ -7,16 +7,16 @@
 set -e
 
 echo -n "Checking bun version in Dockerfile"
-NEW_BUN_VERSION=$(sed -n 's|.*FROM oven/bun:\(.*\)-alpine AS base|\1|p' Dockerfile)
+NEW_BUN_VERSION=$(sed -n 's|^FROM oven/bun:\(.*\)-alpine AS base$|\1|p' Dockerfile)
 if [ -z "$NEW_BUN_VERSION" ]; then
-  echo "Cannot determine bun version from Dockerfile" >&2
+  echo -e "\nCannot determine bun version from Dockerfile" >&2
   exit 1
 else
-  echo "Updating all the other buns to $NEW_BUN_VERSION"
+  echo "...found ${NEW_BUN_VERSION}"
 fi
 
-echo "Updating Bun version in .bun-version"
-echo "$BUN_VERSION" > .bun-version
+echo "Updating bun version in .bun-version"
+echo "$NEW_BUN_VERSION" > .bun-version
 echo
 
 echo "Updating bun version for @types/bun"
