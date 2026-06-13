4d550aa23372a6cc63ba53c354c60fad1eff8808
Bernhard Frick <bernhard.frick@adevinta.com>
2026-06-13 23:52:12 +0200

    add pyroscope

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,20 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
+    console.log("INSTRUMENTATION");
     await import("./instrumentation.node");
   }
+
+  const Pyroscope = require("@pyroscope/nodejs");
+
+  Pyroscope.init({
+    serverAddress: "http://localhost:4040",
+    appName: "volunteer-scheduler",
+    // Enable CPU time collection for wall profiles
+    // This is required for CPU profiling functionality
+    wall: {
+      collectCpuTime: true,
+    },
+  });
+
+  Pyroscope.start();
 }
