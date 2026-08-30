95514081392ec60bb18fba0068726b2df139fd50
Richard Boomsma <richard.boomsma@proton.me>
2026-08-30 22:33:16 +0200

    build: update dependencies

--- src/utils/utc/last-day-of-month.spec.ts
+++ src/utils/utc/last-day-of-month.spec.ts
@@ -1,11 +1,12 @@
 import { expect, test } from "bun:test";
+import { UTCDate } from "@date-fns/utc";
 import { lastDayOfMonthUTC } from "./last-day-of-month";
 
 test("last day of month", () => {
   [
     {
       input: new Date("July 17, 2025, 13:24:00"),
-      expect: new Date("July 31, 2025, 00:00:00.000Z"),
+      expect: new UTCDate("July 31, 2025, 00:00:00.000Z"),
     },
   ].forEach((c) => {
     expect(lastDayOfMonthUTC(c.input)).toEqual(c.expect);
