257d31c6712b084d33e11569b1152e7f9c4208be
Richard Boomsma <richard.boomsma@proton.me>
2026-08-30 22:33:16 +0200

    build: update dependencies

--- src/utils/utc/start-of-month.spec.ts
+++ src/utils/utc/start-of-month.spec.ts
@@ -1,11 +1,12 @@
 import { expect, test } from "bun:test";
+import { UTCDate } from "@date-fns/utc";
 import { startOfMonthUTC } from "./start-of-month";
 
 test("start of of month", () => {
   [
     {
       input: new Date("July 17, 2025, 13:24:00"),
-      expect: new Date("July 1, 2025, 00:00:00.000Z"),
+      expect: new UTCDate("July 1, 2025, 00:00:00.000Z"),
     },
   ].forEach((c) => {
     expect(startOfMonthUTC(c.input)).toEqual(c.expect);
